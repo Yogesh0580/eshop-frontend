3adcfbb21dec6ce9ded863da0774eaf227a60bdd
Yogesh0580 <ydvyogesh9@gmail.com>
2023-05-07 10:46:01 +0530

    final code

---
+++ frontend/src/server.js
@@ -0,0 +1,14 @@
+// export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
+
+// export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
+
+
+// export const server = "http://localhost:8000/api/v2";
+
+// export const backend_url = "http://localhost:8000/"
+
+
+
+export const server = "https://eshop-backend-3gbyr0c5o-yogesh0580.vercel.app/api/v2";
+
+export const backend_url = "https://eshop-backend-3gbyr0c5o-yogesh0580.vercel.app/"
